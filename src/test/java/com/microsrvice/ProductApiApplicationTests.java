389e5067b0f3a642926c129615cf356990f29180
shiv narayan <spnarayan26@gmail.com>
2021-09-06 13:17:19 +0530

    new db

--- src/test/java/com/microsrvice/ProductApiApplicationTests.java
+++ src/test/java/com/microsrvice/ProductApiApplicationTests.java
@@ -1,7 +1,14 @@
 package com.microsrvice;
 
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.microsrvice.model.Orders;
+import com.microsrvice.repository.OrdersRepository;
 
 @SpringBootTest
 class ProductApiApplicationTests {
@@ -9,5 +16,7 @@ class ProductApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	
 
 }
